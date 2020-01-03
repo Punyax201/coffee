46567a8c2f23759617f12f664e69c89ea96b5351
Punyax201 <37551721+Punyax201@users.noreply.github.com>
2020-01-03 09:08:23 +0530

    Final

--- store/data.js
+++ store/data.js
@@ -1,12 +1,3 @@
 export const state = () => ({
-    galleryImages: [
-        {
-            url: require('@/assets/images/coffee/capuccino.jpg'),
-            text:""
-        },
-        {
-            url: require('@/assets/images/coffee/espresso.jpg'),
-            text:""
-        }
-    ]
+    
   })
